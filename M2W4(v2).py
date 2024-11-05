ac3d0df43b88282ca0687b2b5a2707314af923ec
Russel-DH <44677975+Russel-DH@users.noreply.github.com>
2024-11-05 23:21:25 +0300

    Update M2W4(v2).py

--- M2W4(v2).py
+++ M2W4(v2).py
@@ -6,7 +6,7 @@ for i in range(0, len(numbers)):
         print('numbers[i] = ', numbers[i], '; (i = ', i, '), "1" - не простое и не составное')
         i += 1
     else:
-        for j in range(2, numbers[-1]):
+        for j in range(2, numbers[-2]):
             if numbers[i] % j == 0 and numbers[i] > j:
                 print(numbers[i], '- составное, так как numbers[i]/j = ', numbers[i], '/', j, '= ',numbers[i]/j)
                 not_primes.append(numbers[i])
@@ -14,6 +14,6 @@ for i in range(0, len(numbers)):
             elif numbers[i] / j == 1:
                 print(numbers[i], '- простое, так как numbers[i]/j = ', numbers[i], '/', j, '= ', numbers[i]/j)
                 primes.append(numbers[i])
-                #break
+                break
 print('primes: ', primes)
 print('not primes: ', not_primes)
